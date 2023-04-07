8137f86d4f36ca17fab3fab5e9371d50d63083ba
tumbleweed <tumbleWeed_06@proton.me>
2023-04-07 20:37:02 +0530

    html for home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import './styles.css';
+
+const mainBody = document.querySelector('.main-body');
+const projectCreatorBtn = document.querySelector('#project-creator-btn');
+
+function projectCreator(title,description,dueDate,priority,note) {
+    return {title,description,dueDate,priority,note}
+};
+
+function projectDivCreator() {
+    const projectDiv = document.createElement('div');
+    mainBody.append(projectDiv);
+}
 
