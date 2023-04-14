70c4088610eaeec069cd67993b6c882b6aff2307
tumbleweed <tumbleWeed_06@proton.me>
2023-04-14 19:16:12 +0530

    todo div and del btn works for multiple projects

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const mainBody = document.querySelector('.main-body');
 // Projects
 const arrOfProjectDivs = [];
 const arrOfProjects = [];
-let curProjectIndex = -1;
+let curProjectIndex;
 let i = 0;
 
 let testBool = false;
@@ -24,6 +24,7 @@ function projectDivCreator(projectTitle) {
     const projectDiv = document.createElement('li');
     projectSidebarUl.append(projectDiv);
     projectDiv.textContent = projectTitle;
+    projectDiv.className = "project-div"
     arrOfProjectDivs.push(projectDiv);
 }
 
@@ -100,7 +101,6 @@ function todoListCreator(projectName,indexOfProject) {
             
 
             testBool = true;
-            console.log(testBool)
             /** const allTodos = document.querySelectorAll('.main-body div');
             allTodos.forEach((a) => {
                 /** a.addEventListener('click',(todoEvent) => {
@@ -171,9 +171,7 @@ projectCreatorBtn.addEventListener('click', () => {
             })
 
             curProjectIndex = indexOfProject;
-            
-
-            divEvent.stopPropagation()
+            // divEvent.stopPropagation()
             /** console.log(testBool)
             const allTodos = document.querySelectorAll('.main-body div');
 
@@ -189,9 +187,20 @@ projectCreatorBtn.addEventListener('click', () => {
 });
 function testFunc(event) {
     const element = event.target;
+    if(element.classList.contains("project-div")) {
+        
+        console.log(element)
+        if(arrOfProjects[curProjectIndex].arrOfTodos.length === 0) {
+            i = 0;
+        } else {
+            i = arrOfProjects[curProjectIndex].arrOfTodos.length -1;
+        }
+        console.log("i = ",i);
+    }
+    
+    event.stopPropagation();
+    
     if(element.classList.contains("delele-btn")) {
-        
-        
         const indexOfObj = element.id.slice(-1);
         const curObj = arrOfProjects[curProjectIndex].arrOfTodos[indexOfObj];
         curObj.div.remove()
@@ -212,61 +221,7 @@ function testFunc(event) {
         
         console.log(arrOfProjects)
         
-        /* arrOfProjects[curProjectIndex].arrOfTodos.splice(indexOfObj,1);
-        arrOfProjects[curProjectIndex].arrOfTodos[indexOfObj].div.remove();* */
-        // arrOfProjects[curProjectIndex].arrOfTodos[im]
-
-        /** const allDelBtns = document.querySelectorAll('#todo-del-btn');
-            allDelBtns.forEach((e) => {
-                e.addEventListener('click',() => {
-                    if(arrOfProjects[curProjectIndex].arrOfTodos.length === 1) {
-                        i = 0;
-                    }
-                    const curObj = arrOfProjects[curProjectIndex].arrOfTodos[(e.className).slice(-1)];
-                    const indexOfObj = (e.className).slice(-1);
-
-                    console.log(curObj)
-                    console.log(indexOfObj)
-                    console.log(arrOfProjects[curProjectIndex].arrOfTodos[indexOfObj])
-                    arrOfProjects[curProjectIndex].arrOfTodos[indexOfObj].div.remove()
-                    arrOfProjects[curProjectIndex].arrOfTodos.splice(indexOfObj,1);
-                })
-            })* */
-
-    }
-
-        /** const allTodos = document.querySelectorAll('.main-body div');
-            allTodos.forEach((a) => {
-                /** a.addEventListener('click',(todoEvent) => {
-                    const curDivIndex = (a.className).slice(-1);
-                    const todo = arrOfProjects[indexOfProject].arrOfTodos[curDivIndex];
-                    const curDeleteBtn = arrOfProjects[indexOfProject].arrOfTodos[curDivIndex].deleteBtn
-                    console.log(curDeleteBtn)
-
-                    console.log('accessing div')
-                    todoEvent.stopPropagation()
-
-                    
-                })
-                const curDivIndex = (a.className).slice(-1);
-                const todo = arrOfProjects[indexOfProject].arrOfTodos[curDivIndex];
-                const curDeleteBtn = arrOfProjects[indexOfProject].arrOfTodos[curDivIndex].deleteBtn
-
-                curDeleteBtn.addEventListener('click',(delEvent) => {
-                        
-                    // console.log(arrOfProjects[indexOfProject].arrOfTodos[curDivIndex])
-                   todo.div.remove();
-
-                   arrOfProjects[indexOfProject].arrOfTodos.splice(curDivIndex,1);
-
-                    console.log(arrOfProjects)
-                   delEvent.stopPropagation();
-                    
-                })
-            })
-    /** if(arrOfProjectDivs.length !== 0) {
-        
-    }* */
+    }    
 }
 
 document.addEventListener( "click", testFunc );
